e1f0ec858112ec60c37ca680501ab3c88352a40b
Eduardo-Mellado013 <eduardom71891@gmail.com>
2025-02-23 15:53:28 -0800

    Removed Files

--- fonts.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.font_manager as fm
-
-def get_available_fonts():
-    font_families = set([f.name for f in fm.fontManager.ttflist])
-    return font_families
-
-available_fonts = get_available_fonts()
-for font in sorted(available_fonts):
-    print(font)
